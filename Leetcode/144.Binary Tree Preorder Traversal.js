d0ef637692cbe817bd020d4d403aeaef1419d27f
shirujin <rujin_shi@163.com>
2019-06-13 22:18:12 +0800

    先序遍历 非递归

--- Leetcode/144.Binary Tree Preorder Traversal.js
+++ Leetcode/144.Binary Tree Preorder Traversal.js
@@ -21,3 +21,28 @@ var preorderTraversal = function(root) {
     .concat(preorderTraversal(root.left))
     .concat(preorderTraversal(root.right));
 };
+
+/**
+ *
+ * @param {TreeNode} root
+ * @return {number[]}
+ * @description 二叉树前序遍历 非递归
+ * 根节点入栈，保存其值。如果有右节点，则入栈；如果有左节点，则入栈，循环
+ */
+var preorderTraversal = function(root) {
+  if (!root) return [];
+  const res = [];
+  const stack = [root];
+  let t = stack.pop();
+  while (t) {
+    res.push(t.val);
+    if (t.right) {
+      stack.push(t.right);
+    }
+    if (t.left) {
+      stack.push(t.left);
+    }
+    t = stack.pop();
+  }
+  return res;
+};
